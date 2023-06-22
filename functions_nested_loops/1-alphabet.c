e83a24b67d983c1f4873061f1f10a6b2eed5acba
hakimbouharb <wisedaniel45@gmail.com>
2023-06-22 03:02:36 -0700

    update

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *  main-prints putchar
+ * Return:0
+ */
+int main(void)
+{
+	int a = 'a';
+	int z = 'z';
+
+	while (a <= z)
+{
+	_putchar(a);
+	_putchar('\n');
+	a++;
+}
+	return (0);
+}
+
+
