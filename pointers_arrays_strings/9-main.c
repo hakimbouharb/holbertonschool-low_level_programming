94c1726dedcbbe665781ab02d5613efb811d0d84
hakimbouharb <wisedaniel45@gmail.com>
2023-07-02 04:44:22 -0700

    strcpy

---
+++ pointers_arrays_strings/9-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - Prints n elements of an array of integers
+ * @a: Array to be used
+ * @n: Size of array
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%i", a[i]);
+		if (i < n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copies string pointed to by src
+ * @dest: Array being copied to
+ * @src: String to be copied
+ *
+ * Return: The value of pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len;
+	int i = 0;
+
+	while (*src++)
+		i++;
+	len = i;
+	for (i = 0; i <= len; i++)
+		dest[i] = src[i];
+	return (dest);
+}
