887a500dbb2957e1456758b10ca1f2d7f155ec3a
hakimbouharb <wisedaniel45@gmail.com>
2023-07-02 11:29:12 -0700

    0

--- pointers_arrays_strings/0-main.c
+++ pointers_arrays_strings/0-main.c
@@ -6,7 +6,7 @@
  *
  * Return: Always 0.
  */
-int main(void)
+int main(void){
 {
     int n;
 
@@ -15,4 +15,24 @@ int main(void)
     reset_to_98(&n);
     printf("n=%d\n", n);
     return (0);
+}
+{
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
+
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strcat(s1, s2);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
+    return (0);
+}
 }
