8273bf3e2ce9abbb63d4961ee558ba89538bf43a
hakimbouharb <wisedaniel45@gmail.com>
2023-07-02 13:36:58 -0700

    hello 5

--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -6,7 +6,7 @@
  *
  * Return: Always 0.
  */
-int main(void)
+int main(void){
 {
     char s[10] = "My School";
 
@@ -14,4 +14,14 @@ int main(void)
     rev_string(s);
     printf("%s\n", s);
     return (0);
+}
+{
+    char str[] = "Look up!\n";
+    char *ptr;
+
+    ptr = string_toupper(str);
+    printf("%s", ptr);
+    printf("%s", str);
+    return (0);
+}
 }
