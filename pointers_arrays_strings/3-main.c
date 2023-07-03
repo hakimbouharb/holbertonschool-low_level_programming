a6a3018ee38c30583d9e6445f260d565116790b8
hakimbouharb <wisedaniel45@gmail.com>
2023-07-03 03:25:08 -0700

    hello3

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -1,26 +1,18 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main(void){
+int main(void)
 {
-    char *str;
+    char *s = "hello, world";
+    char *f = "oleh";
+    unsigned int n;
 
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    _puts(str);
+    n = _strspn(s, f);
+    printf("%u\n", n);
     return (0);
-}
-
-{
-    char s1[] = "Hello";
-    char s2[] = "World!";
-
-    printf("%d\n", _strcmp(s1, s2));
-    printf("%d\n", _strcmp(s2, s1));
-    printf("%d\n", _strcmp(s1, s1));
-    return (0);
-}
 }
