4c4f6eb24303d9a2eab12f39bbfad75550225796
hakimbouharb <wisedaniel45@gmail.com>
2023-07-03 03:15:06 -0700

    main

--- pointers_arrays_strings/0-main.c
+++ pointers_arrays_strings/0-main.c
@@ -2,37 +2,45 @@
 #include <stdio.h>
 
 /**
- * main - check the code 
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
  *
- * Return: Always 0.
+ * Return: Nothing.
  */
-int main(void){
+void simple_print_buffer(char *buffer, unsigned int size)
 {
-    int n;
+        unsigned int i;
 
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
+        i = 0;
+        while (i < size)
+        {
+                if (i % 10)
+                {
+                        printf(" ");
+                }
+                if (!(i % 10) && i)
+                {
+                        printf("\n");
+                }
+                printf("0x%02x", buffer[i]);
+                i++;
+        }
+        printf("\n");
 }
-{
+
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
+int main(void)
+{
+    char buffer[98] = {0x00};
 
-    char s1[98] = "Hello ";
-    char s2[] = "World!\n";
-    char *ptr;
-
-    printf("%s\n", s1);
-    printf("%s", s2);
-    ptr = _strcat(s1, s2);
-    printf("%s", s1);
-    printf("%s", s2);
-    printf("%s", ptr);
+    simple_print_buffer(buffer, 98);
+    _memset(buffer, 0x01, 95);
+    printf("-------------------------------------------------\n");
+    simple_print_buffer(buffer, 98);    
     return (0);
-}
 }
