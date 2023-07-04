e6be2fb7b6eb9cb2cfa5b5c75cd8354b2fd604c1
hakimbouharb <wisedaniel45@gmail.com>
2023-07-04 05:54:38 -0700

    static libraries

---
+++ static_libraries/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: Variable being evaluated
+ *
+ * Return: Length of string
+ */
+int _strlen(char *s)
+{
+        int i = 0;
+
+        while (*s++)
+                i++;
+        return (i);
+}
+
